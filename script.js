ab9f087a5b7f4ae06d1eac74f0bfc1688ef5556b
Noor Abunamus <noorin.sa.99@gmail.com>
2021-03-25 16:08:04 -0700

    resolving empty labels appearance when the add button is clicked

--- script.js
+++ script.js
@@ -4,37 +4,42 @@ const toDobutton = document.querySelector(".todo-button");
 const toDolist = document.querySelector(".todo-lists")
 
 // Events listener
-toDobutton.addEventListener("click", addTodo);
+toDobutton.addEventListener("click", addTodo , true);
 toDolist.addEventListener("click" , checkNdelete) ;
 
 // function 
 function addTodo(e){
-    if (toDoinput.value.length >0){
-        e.preventDefault();
-    //div
-    const todoDiv = document.createElement("div");
-    todoDiv.classList.add("todo");
-    // li 
-    const NewTodo = document.createElement("li")
-    NewTodo.innerText = toDoinput.value
-    // the value we need to put inside the list 
-    NewTodo.classList.add("todo-item");
-    todoDiv.appendChild(NewTodo);
-    saveTodolocally(toDoinput.value)
-    // check button 
-    const completedButton = document.createElement("button");
-    completedButton.innerHTML = "<i class='fas fa-check'></i>";
-    completedButton.classList.add("complete-btn");
-    todoDiv.appendChild(completedButton);
-    // Deleted button 
-    const trashButton = document.createElement("button");
-    trashButton.innerHTML = "<i class='far fa-trash-alt'></i>";
-    trashButton.classList.add("trash-btn");
-    todoDiv.appendChild(trashButton);
-    // uppend to list 
-    toDolist.appendChild(todoDiv);
-    // clear to do input value 
-    toDoinput.value = " "
+    e.preventDefault();
+    if(toDoinput.value){
+        //div
+        const todoDiv = document.createElement("div");
+        todoDiv.classList.add("todo");
+        
+        // li 
+        const NewTodo = document.createElement("li")
+        NewTodo.textContent = toDoinput.value
+    
+        // the value we need to put inside the list 
+        NewTodo.classList.add("todo-item");
+        todoDiv.appendChild(NewTodo);
+        saveTodolocally(toDoinput.value)
+   
+        // check button 
+       const completedButton = document.createElement("button");
+       completedButton.innerHTML = "<i class='fas fa-check'></i>";
+       completedButton.classList.add("complete-btn");
+       todoDiv.appendChild(completedButton);
+
+      // Deleted button 
+      const trashButton = document.createElement("button");
+      trashButton.innerHTML = "<i class='far fa-trash-alt'></i>";
+      trashButton.classList.add("trash-btn");
+      todoDiv.appendChild(trashButton);
+    
+      // uppend to list 
+      toDolist.appendChild(todoDiv);
+     // clear to do input value 
+      toDoinput.value = null;
     }
     
 }
@@ -53,6 +58,7 @@ function checkNdelete(e) {
         // toggle : checks the selected elements for visibility
         todo.classList.toggle("completed");}
 }
+
 function saveTodolocally(todo){
     let todos ;
     if(localStorage.getItem("todos") === null){
@@ -63,5 +69,5 @@ function saveTodolocally(todo){
     }
     todos.push(todo)
     // saving
-    localStorage.setItem("todos" , JSON.stringify(todos))
+    localStorage.setItem("todos" , JSON.stringify(todos));
 }
