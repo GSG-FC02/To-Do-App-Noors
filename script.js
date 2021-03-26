5eeef8aa5261e1db4598d3fb823b76a17a2820fc
Noor Abunamus <noorin.sa.99@gmail.com>
2021-03-26 14:34:15 -0700

    remove deleted items from local storage and take the stored items to stick it on the app when realoding the page process is going on

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const toDolist = document.querySelector(".todo-lists")
 // Events listener
 toDobutton.addEventListener("click", addTodo , true);
 toDolist.addEventListener("click" , checkNdelete) ;
+document.addEventListener("DOMContentLoaded", getTodos);
 
 // function 
 function addTodo(e){
@@ -50,13 +51,15 @@ function checkNdelete(e) {
     //delete 
     if (item.classList[0] === "trash-btn"){
         const todo = item.parentElement;
-        todo.remove()   
+        todo.remove();
+        removelocal(todo);  
     }
     // checkmark 
     if (item.classList[0] === "complete-btn"){
         const todo = item.parentElement;
         // toggle : checks the selected elements for visibility
-        todo.classList.toggle("completed");}
+        todo.classList.toggle("completed");
+       }
 }
 
 function saveTodolocally(todo){
@@ -70,4 +73,62 @@ function saveTodolocally(todo){
     todos.push(todo)
     // saving
     localStorage.setItem("todos" , JSON.stringify(todos));
+}
+
+function getTodos(){
+    let todos ;
+    if(localStorage.getItem("todos") === null){
+        todos = [];
+    }
+    else{
+        todos = JSON.parse(localStorage.getItem("todos"))
+    }
+    todos.forEach(
+        function(todo){
+        //div
+        const todoDiv = document.createElement("div");
+        todoDiv.classList.add("todo");
+        // li 
+        const NewTodo = document.createElement("li");
+        NewTodo.textContent = todo;
+    
+        // the value we need to put inside the list 
+        NewTodo.classList.add("todo-item");
+        todoDiv.appendChild(NewTodo);
+        // check button 
+        const completedButton = document.createElement("button");
+        completedButton.innerHTML = "<i class='fas fa-check'></i>";
+        completedButton.classList.add("complete-btn");
+        todoDiv.appendChild(completedButton);
+
+        // Deleted button 
+        const trashButton = document.createElement("button");
+        trashButton.innerHTML = "<i class='far fa-trash-alt'></i>";
+        trashButton.classList.add("trash-btn");
+        todoDiv.appendChild(trashButton);
+    
+       // uppend to list 
+       toDolist.appendChild(todoDiv);
+    });
+
+}
+
+function removelocal(todo){
+    // todo here is a div
+    let todos ;
+    if(localStorage.getItem("todos") === null){
+        todos = [];
+    }
+    else{
+        todos = JSON.parse(localStorage.getItem("todos"))
+    };
+    // console.log(todo)  => access to class "todo"
+    // console.log(todo.children[0].innerText) => access to the text inside li
+    // console.log(todos.indexOf(todo.children[0].innerText)) => to get the index of the text inside the li
+    const todoIndex = todo.children[0].innerText;
+    // (from what positon u wanna remove the element , How many element you wanna remove)
+    todos.splice(todos.indexOf(todoIndex) , 1);
+    // method converts a JavaScript object or value to a JSON string
+    localStorage.setItem("todos" , JSON.stringify(todos)); 
+
 }
